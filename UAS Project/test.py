c99fad50385989610d07a71a2110e8003449bfe0
Shivansh Chauhan <147849037+Programmer-Shivansh@users.noreply.github.com>
2024-02-07 22:14:26 +0530

    Delete UAS Project directory

--- UAS Project/test.py
+++
@@ -1,8 +0,0 @@
-import cv2
-
-e = cv2.imread("E.png", 0)
-_, ne = cv2.threshold(e, 50, 255, cv2.THRESH_BINARY_INV)
-# cv2.imshow("img2",e)
-cv2.imshow("img1", ne)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
